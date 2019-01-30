632692abbe585797bb56f54353af41eaf9286e5c
A-2574 <A-2574@192.168.1.151>
2019-01-30 15:40:52 +0530

    first

--- FlightSearch/FlightSearchNLP/WebContent/app-chrome.js
+++ FlightSearch/FlightSearchNLP/WebContent/app-chrome.js
@@ -31,6 +31,30 @@
 		function setInput(text) {
 			$("#textHide").val(text);
 			var text = $("#textHide").val();
-			typeSearchByString(text);
+			typeSearchWit(text);
 		}
-
+	function typeSearchWit(text){
+			
+			var xhr=new XMLHttpRequest();
+			  xhr.onload=function(e) {
+			      if(this.readyState === 4&&this.status == 200) {
+			          console.log("Server returned: ",e.target.responseText);
+			         var responses= e.target.responseText.split(',');
+			          var from=responses[0].trim();
+			          var dep=responses[1].trim();
+			          var travelDate=responses[2].trim();
+			         
+			        window.open("https://www.google.com/flights?"
+			      	    		+"q="+from+"+to+"+dep+"+on+"+travelDate);
+			        console.log("https://www.google.com/flights?"
+			      	    		+"q="+from+"+to+"+dep+"+on+"+travelDate);
+			      }
+			  };
+			 var fd=new FormData();
+			// var vals=$("#test").attr('src');
+			 // //var urlValue = base64data;
+			  fd.append("searchString",text);
+			 //fd.append("audio_data",blob);
+			  xhr.open("POST","./witapi",true);
+			  xhr.send(fd);
+		}
